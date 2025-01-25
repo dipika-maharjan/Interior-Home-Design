ec541fff006764f4cdd1342929389e5759c45c28
dipika-maharjan <maharjandipika49@gmail.com>
2025-01-25 21:18:17 +0545

    living section done

--- src/components/private/LivingSection.jsx
+++ src/components/private/LivingSection.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import '../../styles/LivingSection.css';
 import FamilyLiving from '../../assets/images/FamilyLiving.jpg';
+import EntertainmentLiving from '../../assets/images/EntertainmentLiving.webp';
+import ReadingNook from '../../assets/images/ReadingNook.jpg';
+import OpenConcept from '../../assets/images/OpenConcept.jpg';
+import SmallSpace from '../../assets/images/SmallSpace.jpg';
+import NatureInspired from '../../assets/images/NatureInspired.jpg';
+import Vintage from '../../assets/images/Vintage.jpg';
+import Eclectic from '../../assets/images/Eclectic.jpg';
+import CoastalCottage from '../../assets/images/CoastalCottage.jpg';
+import LivingSpaceIdeas from '../../assets/images/LivingSpaceIdeas.png'
+
 function LivingSection(){
     return(
         <div>
             <div className="living-wrapper">
                 <div className="living-heading">
-                <h1>Living Spaces Ideas</h1>
-                <p>
-                Living spaces are the heart of the home, where memories are made. <br/>
-                Whether it’s family movie nights, entertaining guests, or relaxing with a book, these rooms should be cozy, functional, and inspiring. <br/>
-                From practical furniture to a warm fireplace, create a space you'll love spending time in.</p>
+                    <img src ={LivingSpaceIdeas} alt="Living Space Ideas"/>
+                    <div className="living-description">
+                    <h1>Living Spaces Ideas</h1>
+                    <p>
+                    Living spaces are the heart of the home, where memories are made. <br/>
+                    Whether it’s family movie nights, entertaining guests, or relaxing with a book, these rooms should be cozy, functional, and inspiring. <br/>
+                    From practical furniture to a warm fireplace, create a space you'll love spending time in.</p>
+                    </div>
                 </div>
 
-                <div className="living-category">
-                    <h1>By Purpose</h1>
-                </div>
 
                 <div className="living-section">
                     <div className="living-box">
@@ -32,7 +42,7 @@ function LivingSection(){
                         <div className="living-content">
                             <h2>Entertainment Living Room</h2>
                             <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
+                            <img src={EntertainmentLiving} alt="Entertainment Living Room" />
                             </div>
                             <p>A media-centric setup with a big-screen TV, sound systems, and cozy seating.</p>
                         </div>
@@ -42,7 +52,7 @@ function LivingSection(){
                         <div className="living-content">
                             <h2>Reading Nook Living Room</h2>
                             <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
+                            <img src={ReadingNook} alt="ReadingNook Living Room" />
                             </div>
                             <p>A peaceful corner with bookshelves, soft lighting, and a comfy chair.</p>
                         </div>
@@ -52,161 +62,65 @@ function LivingSection(){
                         <div className="living-content">
                             <h2>Open-Concept Living Room</h2>
                             <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
+                            <img src={OpenConcept} alt="OpenConcept Living Room" />
                             </div>
-                            <p>Blended spaces with seamless transitions between kitchen, dining, and living areas.</p>
+                            <p> Spacious, airy, and seamlessly connected.</p>
                         </div>
                     </div>
+
+                    <div className="living-box">
+                        <div className="living-content">
+                            <h2>Eclectic Living Room</h2>
+                            <div className="living-image">
+                            <img src={Eclectic} alt="Eclectic Living Room" />
+                            </div>
+                            <p>A mix of diverse styles and colors.</p>
+                        </div>
+                    </div>
+
+                    <div className="living-box">
+                        <div className="living-content">
+                            <h2>Coastal Cottage Living Room</h2>
+                            <div className="living-image">
+                            <img src={CoastalCottage } alt="Family Living Room" />
+                            </div>
+                            <p>A light, airy space with soft blues, and natural textures.</p>
+                        </div>
+                    </div>
+
+                    <div className="living-box">
+                        <div className="living-content">
+                            <h2>Small Space Living Room</h2>
+                            <div className="living-image">
+                            <img src={SmallSpace} alt="SmallSpace Living Room" />
+                            </div>
+                            <p> Compact designs that maximize functionality and style in limited space.</p>
+                        </div>
+                    </div>
+
+                    <div className="living-box">
+                        <div className="living-content">
+                            <h2>Nature-Inspired Living Room</h2>
+                            <div className="living-image">
+                            <img src={NatureInspired} alt="NatureInspired Living Room" />
+                            </div>
+                            <p> A calming space with natural elements and plants</p>
+                        </div>
+                    </div>
+
+                    <div className="living-box">
+                        <div className="living-content">
+                            <h2>Vintage Chic Living Room</h2>
+                            <div className="living-image">
+                            <img src={Vintage} alt="Family Living Room" />
+                            </div>
+                            <p>A nostalgic space with retro furniture and antique decor.</p>
+                        </div>
+                    </div>
+
                 </div>
 
-
-
-
-                <div className="living-category">
-                    <h1>By Theme</h1>
-                </div>
-
-                <div className="living-section">
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Family Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>Kid-friendly, durable furniture, and playful touches for family activities.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Entertainment Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>A media-centric setup with a big-screen TV, sound systems, and cozy seating.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Reading Nook Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>A peaceful corner with bookshelves, soft lighting, and a comfy chair.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Open-Concept Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>Blended spaces with seamless transitions between kitchen, dining, and living areas.</p>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="living-category">
-                    <h1>By Features</h1>
-                </div>
-
-                <div className="living-section">
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Family Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>Kid-friendly, durable furniture, and playful touches for family activities.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Entertainment Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>A media-centric setup with a big-screen TV, sound systems, and cozy seating.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Reading Nook Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>A peaceful corner with bookshelves, soft lighting, and a comfy chair.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Open-Concept Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>Blended spaces with seamless transitions between kitchen, dining, and living areas.</p>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="living-category">
-                    <h1>By Purpose</h1>
-                </div>
-
-                <div className="living-section">
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Family Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>Kid-friendly, durable furniture, and playful touches for family activities.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Entertainment Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>A media-centric setup with a big-screen TV, sound systems, and cozy seating.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Reading Nook Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>A peaceful corner with bookshelves, soft lighting, and a comfy chair.</p>
-                        </div>
-                    </div>
-
-                    <div className="living-box">
-                        <div className="living-content">
-                            <h2>Open-Concept Living Room</h2>
-                            <div className="living-image">
-                            <img src={FamilyLiving} alt="Family Living Room" />
-                            </div>
-                            <p>Blended spaces with seamless transitions between kitchen, dining, and living areas.</p>
-                        </div>
-                    </div>
-                </div>
-
-
-
-
-
-            </div>
+           </div>
         </div>
     )
 }
